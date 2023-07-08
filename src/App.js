0739f7902f7ab892713cc646acfff0a656bb7bb2
achintya <achintyammishra2000@gmail.com>
2023-07-08 15:16:13 +0530

    initial commit navbar done

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Navbar from "./Navbar/Navbar";
+import "./index.css";
+import { MdOutlineNightlight, MdLightMode } from "react-icons/md";
+import logo_bright from "./imgs/A.png";
+import logo_dark from "./imgs/A2.png";
+function App() {
+  const navLinks = ["Home", "About", "Posts", "Contact"];
+  const [backgroundColor, setBackgroundcolor] = useState(false);
+  const handleBackgroundColor = () => {
+    if (!backgroundColor) {
+      document.body.style.backgroundColor = "hsl(210deg, 30%, 8%)";
+    } else {
+      document.body.style.backgroundColor = "#fff";
+    }
+    setBackgroundcolor(!backgroundColor);
+  };
+  return (
+    <div className="container">
+      {backgroundColor ? (
+        <Navbar
+          navLinks={navLinks}
+          changeBackgroundColor={handleBackgroundColor}
+          color="white"
+          themeIcon={
+            <MdOutlineNightlight
+              className="custom-icon"
+              style={{
+                width: "60px",
+                height: "30px",
+                color: "white",
+              }}
+            />
+          }
+          backgroundColor="hsl(210deg, 30%, 8%)"
+          logo={logo_dark}
+        />
+      ) : (
+        <Navbar
+          navLinks={navLinks}
+          changeBackgroundColor={handleBackgroundColor}
+          color="#222"
+          themeIcon={
+            <MdLightMode
+              className="bright-icon"
+              style={{ width: "60px", height: "30px" }}
+            />
+          }
+          logo={logo_bright}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar/Navbar.js
@@ -0,0 +1,44 @@
+import "./Navbar.css";
+
+function Navbar({
+  navLinks,
+  changeBackgroundColor,
+  color,
+  themeIcon,
+  backgroundColor,
+  logo,
+}) {
+  const renderLinks = navLinks.map((links) => {
+    return (
+      <li class="nav-links-list">
+        <button className="nav-links" style={{ color, backgroundColor }}>
+          {links}
+        </button>
+      </li>
+    );
+  });
+  return (
+    <div className="nav-container">
+      <nav className="navbar">
+        <p className="logo-container">
+          <a
+            href="https://www.linkedin.com/in/achintya-mishra-760604155"
+            rel="noreferrer"
+            target="_blank"
+            className="logo-tail logo-tail-render"
+          >
+            Achintya
+          </a>
+        </p>
+        <ul className="navbar-list">{renderLinks}</ul>
+        <ul className="navbar-list icon-list">
+          <li onClick={() => changeBackgroundColor()} className="nav-icon">
+            {themeIcon}
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
