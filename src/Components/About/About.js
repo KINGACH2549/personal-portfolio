885b71f946d1d303cf081ff666abd250867f95e3
achintya <achintyammishra2000@gmail.com>
2023-07-18 19:14:11 +0530

    added exp section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,18 +1,46 @@
 import "./About.css";
+import { Player } from "@lottiefiles/react-lottie-player";
+import { BsArrowUpRight } from "react-icons/bs";
 
-function About({ skills, className }) {
-  const renderedButtons = skills.map((skill) => {
-    return <button className={className}>{skill}</button>;
+function About({ skills, className, expereinces, color }) {
+  const experienceCards = expereinces.map((experience) => {
+    return (
+      <div className="expereince-card" style={{ color }}>
+        <header className="exp-date" style={{ color }}>
+          {experience.fromdate}-{experience.todate}
+        </header>
+        <div className="exp-header">
+          <h3 className="exp-des">{experience.designation}</h3>
+          <h4 className="exp-company">{experience.company}</h4>
+        </div>
+        <BsArrowUpRight
+          className="exp-arrow"
+          style={{
+            position: "absolute",
+            height: "50px",
+            width: "30px",
+            top: "10px",
+            right: "20px",
+          }}
+        />
+        <div className="exp-desc">{experience.Description}</div>
+        <ul className="exp-tech">
+          {experience.TechStack.map((skill) => {
+            return <li>{skill}</li>;
+          })}
+        </ul>
+      </div>
+    );
   });
   return (
     <div className="about-container">
       <p className="about-content-container">
         <span className="about-content">
-          "I'm , a recent undergraduate who's genuinely passionate about
-          building systems at scale. I have a deep love for technology and its
-          potential to transform lives. My academic journey has been great, and
-          I have achieved a CGPA of 8.97 during my undergrad. During my time as
-          a backend development intern
+          I'm , a recent undergraduate who's genuinely passionate about building
+          systems at scale. I have a deep love for technology and its potential
+          to transform lives. My academic journey has been great, and I have
+          achieved a CGPA of 8.97 during my undergrad. During my time as a
+          backend development intern
         </span>
 
         <span className="about-content">
@@ -21,16 +49,28 @@ function About({ skills, className }) {
           contribute to meaningful projects. I truly believe that my
           capabilities enable me to contribute to the development of large-scale
           systems. I'm excited about the future and the potential to make a
-          positive impact with my work"
+          positive impact with my work
         </span>
       </p>
       <div className="skills-experience-container">
-        <div className="skills-grid">
+        {/* <div className="skills-grid">
           <p>skills</p>
           {renderedButtons}
-        </div>
-        <div>
+        </div> */}
+        <Player
+          src="https://lottie.host/ee2c3280-5981-4340-a7a5-4bccc878c821/DlivVtso87.json"
+          loop
+          autoplay
+          style={{
+            height: "400px",
+            width: "400px",
+            // borderRadius: "50%",
+            // backgroundColor: "white",
+          }}
+        />
+        <div className="experience-section">
           <p>Experience</p>
+          <div className="overflow-exp"> {experienceCards}</div>
         </div>
       </div>
     </div>
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,6 +1,17 @@
 import About from "../Components/About/About";
 
 function AboutPage({ backgroundColor }) {
+  const expereinces = [
+    {
+      fromdate: "2023 Jan",
+      todate: "2023 May",
+      Description:
+        "  Created analytics platform to collect users interaction data on any website and also developed client-side plugin for it using client-side Java-Script. Integrated Razorpay Payment Gateway for their clients using RazorPay API’s and Webhooks. Also developed complete backend of companies in house product called bio-1 which let users to create their own portfolio websites with proper no code solution. ",
+      TechStack: ["Node js", "Express js", "MySQL"],
+      company: "codes 'n' coffee Tech",
+      designation: "Backend Development Intern",
+    },
+  ];
   const skills = [
     "React js",
     "Node js",
@@ -17,9 +28,19 @@ function AboutPage({ backgroundColor }) {
   return (
     <>
       {backgroundColor ? (
-        <About className="skill-dark" skills={skills} />
+        <About
+          className="skill-dark"
+          skills={skills}
+          expereinces={expereinces}
+          color="#fff"
+        />
       ) : (
-        <About className="skills" skills={skills} />
+        <About
+          className="skills"
+          skills={skills}
+          expereinces={expereinces}
+          color="#222"
+        />
       )}
     </>
   );
